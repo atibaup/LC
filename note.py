195493f9d9a5e549662c85d13577ede37b410cf9
Arnau Tibau-Puig <apuig@walmartlabs.com>
2014-03-02 20:11:49 -0800

    LendingClubNote class

--- note.py
+++ note.py
@@ -1,6 +1,9 @@
 import datetime as dt
 import calendar
 from dateutil.relativedelta import relativedelta
+import sys
+sys.path.append('/Users/apuig/Documents/Projects/lendingclubchecker')
+from lendingclub import *
 
 class Note:
    def __init__(self, principal, interest, term, start=dt.date.today(), fee=0.01):
@@ -80,7 +83,8 @@ class Note:
       for i in xrange(t, self.term):
         print '%d\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f' % (i+1, self.date(i), self.principal(i), 
                                                         self.interest(i), A,
-                                                        self.principal_balance(i+1), self.interest_balance(i+1))
+                                                        self.principal_balance(i+1),
+                                                        self.interest_balance(i+1))
    
    def accrued_interest_at_t(self, t, year, month_num, n_days):
        '''
@@ -89,7 +93,6 @@ class Note:
        Accrued Interest = p(t) * interest * (n_days/daysInMonth) 
        '''
        days_in_month = calendar.monthrange(year, month_num)[1]
-
        return self.principal_balance(t)*self.I*float(n_days)/days_in_month
 
    def face_value_at_t(self, t, year, month_num, n_days):
@@ -151,4 +154,54 @@ class Note:
        for t in xrange(self.term):
            if P_balance >= self.principal_balance(t):
                return self.term - t
+
+class LendingClubNote(Note):
+    '''
+    An extension of Note to the specifics of Lending Club note
+    '''
+    def __init__(self, loan_id, note_id, order_id, principal, interest, grade, term, start=dt.date.today(), fee=0.01, **kwargs):
+        '''
+        Call: LendingClubNote(loan_id, note_id, order_id, principal, interest, term, start=dt.date.today(), fee=0.01)
+        '''
+        Note.__init__(self, principal, interest, term, start, fee)
+        self.loan_id = loan_id
+        self.note_id = note_id
+        self.order_id = order_id
+        self.grade = grade
+        self.collection_log = kwargs.get('collection_log', None)
+        self.payment_history = kwargs.get('payment_history', None)
+        self.credit_history = kwargs.get('credit_history', None)
+        
+    @classmethod
+    def from_website(cls, loan_id, note_id, order_id, lc_browser = None):
+        '''
+
+        '''
+        from urllib import urlopen
+        note_url = 'https://www.lendingclub.com/account/loanPerf.action?loan_id=%d&order_id=%d&note_id=%d' % (
+                                loan_id, order_id, note_id)
+        print 'Attempting to retrieve note details from %s' % note_url
+        if lc_browser is None:
+            lc_browser = LendingClubBrowser()
+        lc_browser.login()
+        soup = BeautifulSoup(lc_browser.browser.open(note_url).read())
+        
+        credit_history = extract_credit_history(soup)
+        collection_log = extract_collection_log(soup)
+        payment_history = extract_payment_history(soup)
+        start_date, principal, grade, interest, term = extract_note_info(soup)
+        return cls(loan_id, note_id, order_id, principal, interest, grade, term, start_date, collection_log = collection_log,
+                 credit_history = credit_history, payment_history = payment_history)
+
+
+def extract_note_info(soup):
+    note_details = [extract_row(r) for a in soup.findAll('div', {'id': 'object-details'}) for r in a.findAll('table')][0]  
+    start_date = parsedate(note_details[0])
+    principal = int(note_details[1].split('$')[1])
+    grade = note_details[3].split('&nbsp;:&nbsp;')[0]
+    interest = float(re.sub('%', '', note_details[3].split('&nbsp;:&nbsp;')[1]))
+    term = int(note_details[4].split('&nbsp;&nbsp;')[0])
+    return start_date, principal, grade, interest, term
+
+
 
